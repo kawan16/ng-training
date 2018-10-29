de284f652ce2523ea6b7a18e2b5d6a38ebd7963b
Karl <karl.devooght@gmail.com>
2018-10-29 21:46:20 +0100

    step-02

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DisplayHeaderComponent } from './display-header/display-header.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DisplayHeaderComponent]
+})
+export class CoreModule { }
---
+++ src/app/core/display-header/display-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-header',
+  templateUrl: './display-header.component.html',
+  styleUrls: ['./display-header.component.css']
+})
+export class DisplayHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
